349c8c74fd40821fd83cd0fae52b9579c90da6f5
HakureiSino <sino@linux-zmsy.workgroup>
2015-12-27 11:15:34 +0800

    Update

--- loginshanxun.sh
+++ loginshanxun.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
+#Author:Hakurei Sino
+#Version:1.0
+#Fuction:Login SingleNet in Linux
 
 getchar() {
+#Password Protect
     stty cbreak -echo
     dd if=/dev/tty bs=1 count=1 2> /dev/null
     stty -cbreak echo
 }
-attrget()  
-{  
-        ATTR_PAIR=${1#*$2=\"}  
-        echo "${ATTR_PAIR%%\"*}"  
-}  
+
+#const URL to finish the first step certification
 ShowLoginUrl="http://115.239.134.163:8080/showlogin.do"
 
 echo "UserName:"
@@ -24,8 +25,15 @@ while : ; do
     fi
     PassWord="$PassWord$ret"
 done
+
+#Module : EncryitPassword 
+#Function : parse the password and Encrypt it
+#Need Java Environment
 EPass=$(java EncryptPassword PassWord)
 ipaddress=$(sudo ifconfig wlan0|grep "inet addr"|cut -d: -f2|awk '{print $1}')
+
+#First CURL get Uuid and LoginURL
+#More INFORMATION in README
 result1=$(curl -H "User-Agent:China Telecom Client" -d "wlanuserip=$ipaddress" $ShowLoginUrl)
 echo "[Handling Return content]"
 UserIP=$(echo $result1|awk -vFS="<UserIP>" '{print $2}'|awk -vFS="</UserIP>" '{print $1}')
@@ -35,6 +43,9 @@ if [[ x$UserIP != x$ipaddress ]]||[[ x$Uuid = x ]]||[[ x$LoginURL == x ]]; then
 	echo returnValueError , Please Login Again Later.
 	exit 1
 fi
+
+#Second CURL regist user in server
+#and get LogoffURL 
 echo UserName is $UserName , IpAddress is $ipaddress Logining
 result2=$(curl -H "User-Agent:China Telecom Client" -d \
 	"username=$UserName\
